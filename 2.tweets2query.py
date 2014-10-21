69aa7a07d043c147d8af39b67d1d00da9581ce28
Weilin Xu <mzweilin@gmail.com>
2014-10-21 16:23:46 -0400

    Update module2 and the sample data.

--- 2.tweets2query.py
+++ 2.tweets2query.py
@@ -1,13 +1,69 @@
+import string, nltk
+from collections import Counter
+from nltk.stem.snowball import EnglishStemmer
+from wordsegment import segment
 
-
-
-output = "twitter blade"
+stemmer = EnglishStemmer()
+tokenizer = nltk.tokenize.treebank.TreebankWordTokenizer()
+punc_list = list(string.punctuation)
 
 class QueryGenerator:
-    def __init__(self, freq_n_grams, top_k_terms):
-        self.freq_n_grams = freq_n_grams
-        self.top_k_terms = top_k_terms
+    def __init__(self, ngrams_tops = {1:3, 2: 1, 3:1}):
+        #self.freq_n_grams = freq_n_grams
+        #self.top_k_terms = top_k_terms
+        self.ngrams_tops = {1:3, 2: 1, 3:1}
+        self.counters = {}
 
+    def gen_query_list(self, hashtag, tweets):
+        q_list = []
+        q_list.append(hashtag)
+        q_list.append(segment(hashtag[1:]))
+
+        doc_str = ""
+        for tweet in tweets:
+            doc_str += tweet['content'] + ' '
+
+        self.analyse_doc(doc_str)
+
+        for (n, tops) in self.ngrams_tops.items():
+            q_list.append(self.get_sorted_tokens(n, tops))
+
+        return q_list
+
+    def analyse_doc(self, doc):
+        tokens = self.str2tokens(doc)
+
+        for n in self.ngrams_tops.keys():
+            self.counters[n] = Counter()
+            self.counters[n].update(self.tokens2ngrams(tokens, n))
+
+    def get_sorted_counts(self, ngram, tops = None):
+        return [tup[1] for tup in self.counters[ngram].most_common(tops)]
+    
+    def get_sorted_tokens(self, ngram, tops = None):
+        return [tup[0] for tup in self.counters[ngram].most_common(tops)]
+
+    def str2tokens(self, doc_str):
+        tokens = []
+        for token in tokenizer.tokenize(doc_str):
+            if token in punc_list:
+                continue
+            token_norm = stemmer.stem(token.lower())
+            tokens.append(token_norm)
+        return tokens
+
+    def tokens2ngrams(self, tokens, n):
+        if n == 1:
+            return tokens
+        
+        ret = []
+        for i in range(0, len(tokens)-n+1):
+            ngrams = []
+            for j in range(0,n):
+                ngrams.append(tokens[i+j])
+            ret.append(' '.join(ngrams))
+        return ret 
+        
     def get_top_grams(self, string):
         return []
 
@@ -20,10 +76,10 @@ class QueryGenerator:
 def test():
     from sample_data import tweet_collection
 
-    qgen = QueryGenerator(freq_n_grams = [1,2,3], top_k_terms = 10)
+    qgen = QueryGenerator()
 
     for (hashtag, tweets) in tweet_collection.items():
-        query_list = qgen.generate_query_list(hashtag, tweets)
+        query_list = qgen.gen_query_list(hashtag, tweets)
         print "Query list for \"%s\" is " % hashtag
         print query_list
 
