e05ad8bd9ff608314bce6d097a0dff4917755e19
Nicholas Janus <nickjanus@gmail.com>
2014-11-18 14:59:20 -0500

    Fixes search example

--- bing_search.py
+++ bing_search.py
@@ -9,10 +9,9 @@ def main():
 def search(query, num_results):
     urls = []
     file_path = "queries/" + query 
-    cmd = "ruby bing_search.rb " + ('%s ' % query) + str(num_results)
+    cmd = "ruby bing_search.rb " + ('"%s" ' % query) + str(num_results) + " > " + file_path.replace(" ","\ ")
     if not os.path.exists(file_path):
-        with open(file_path, mode="w") as f:
-            proc.call(cmd, shell=True, stdout=f)
+        proc.call(cmd, shell=True)
     with open(file_path, mode='r') as f:
         data = json.loads(f.read())
         for site in data[0]['Web']:
