bba6ae24ced459da78d66d20f4c23f3f1f3e8a58
Nicholas Janus <nickjanus@gmail.com>
2014-11-25 11:54:59 -0500

    Fixes search

--- test/test_bing_search.py
+++ test/test_bing_search.py
@@ -1,18 +1,16 @@
-from bing_search_api import BingSearchAPI
+from bing_search_api import BingSearchAPI 
 my_key = "MEL5FOrb1H5G1E78YY8N5mkfcvUK2hNBYsZl1aAEEbE"
- 
-def main():
-    print(search("Hello", 3))
 
-def search(query_string, num_results):
+def query(query_string):
     bing = BingSearchAPI(my_key)
     params = {'ImageFilters':'"Face:Face"',
               '$format': 'json',
-              '$top': num_results,
+              '$top': 10,
               '$skip': 0}
     results = bing.search('web',query_string,params).json() # requests 1.0+ 
 
     return [result['Url'] for result in results['d']['results'][0]['Web']]
- 
+
 if __name__ == "__main__":
-    main()
+    query_string = "Your Query"
+    print query(query_string)
