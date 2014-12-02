cc56bbc6e6896df6f0b3f49f11c86ad31288fb71
Weilin <mzweilin@gmail.com>
2014-12-02 14:29:45 -0500

    Added a parameter of search category, Web/News.

--- lib/search/bing_search.py
+++ lib/search/bing_search.py
@@ -4,15 +4,16 @@ my_key = "MEL5FOrb1H5G1E78YY8N5mkfcvUK2hNBYsZl1aAEEbE"
 def main():
     print(search("Hello", 3))
 
-def search(query_string, num_results):
+# category='Web' or category='News'
+def search(query_string, num_results, category='Web'):
     bing = BingSearchAPI(my_key)
     params = {'ImageFilters':'"Face:Face"',
               '$format': 'json',
               '$top': num_results,
               '$skip': 0}
-    results = bing.search('web',query_string,params).json() # requests 1.0+ 
+    results = bing.search(category,query_string,params).json() # requests 1.0+ 
 
-    return [result['Url'] for result in results['d']['results'][0]['Web']]
+    return [result['Url'] for result in results['d']['results'][0][category]]
  
 if __name__ == "__main__":
     main()
