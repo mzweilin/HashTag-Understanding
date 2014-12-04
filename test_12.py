bddb088d364339d2b3bd754dfb6be2cc5851e579
Weilin <mzweilin@gmail.com>
2014-12-04 15:07:05 -0500

    updated test code.

--- test_12.py
+++ test_12.py
@@ -1,12 +1,23 @@
 from lib.tweet.parseTwitter import retrieveTweetText
 from lib.querygen.tweets2query import QueryGenerator
-    
-hashtag = "twitterblades"
-tweets = retrieveTweetText(hashtag)
+
+import sys
+
+hashtag = '#'+sys.argv[1]
+  
+# hashtag = "#twitterblades"
+# #hashtag = "#uva"
+# hashtag = "#IAD"
+# #hashtag = "#lab14"
+# #hashtag = "#oitnb"
+
+tweets = retrieveTweetText(hashtag, 3, 1, 1, 1)
+for t in tweets:
+    print "==>", t
 
 qgen = QueryGenerator()
 
 
-query_list = qgen.gen_query_list(hashtag, tweets)
+query_list = qgen.gen_query_list(hashtag, tweets, stopwords_filter=True, stemming=False)
 print("Query list for \"%s\" is " % hashtag)
 print(query_list)
