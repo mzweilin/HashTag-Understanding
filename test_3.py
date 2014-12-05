f9ca56545b593fe8c06517f786438c2d41f2482e
Weilin <mzweilin@gmail.com>
2014-12-05 15:59:28 -0500

    Search result merging.

--- test_3.py
+++ test_3.py
@@ -1,13 +1,39 @@
 import lib.search.bing_search as bing
 import string
+from collections import Counter
 
-urls = []
-queries = ['sufc twitterblades', 'blades twitter', '#twitterblades']
+# def match_url_keywords(url, keywords):
+#     sentences = get_sentences_from_url(url)
+#     weights = Counter()
+#     for sentence in sentences:
+#         tokens = tokenize(sentence)
+#         for token in tokens:
+#             if token in keywords:
+#                 weights[token] += 1
+#     return weights
+
+"http://www.wikipedia.org/"
+
+url_counter = Counter()
+# keywords = ['twitter', 'blades', 'sufc']
+#queries = ['#twitterblades', 'sufc', 'twitterblades sufc', 'sufc twitterblades']
+#queries = ['#pangu', 'pangu jailbreak', 'jailbreak pangu']
+queries = ['#pangu', 'jailbreak', 'jailbreak pangu', 'jailbreak pangu']
+#queries = ['']
+#queries = ['#pangu']
 for query in queries:
     if isinstance(query, list):
         query = " ".join(query)
     query = query.replace("#", "%23")
     print query
-    urls = urls + bing.search(query, 5)
-print urls
+    on_wiki = False
+    query += " -site:twitter.com"
+    if on_wiki == True:
+        query += " site:wikipedia.org"
+    urls = bing.search(query, 20)
+    for i in range(len(urls)):
+        url = urls[i]
+        print url
+        url_counter[url] += float(1)/3**float(i)
+print url_counter.most_common(10)
 
