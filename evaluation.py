71190d8a15e3134439429e7d0100a8d1ed142984
Weilin <mzweilin@gmail.com>
2014-12-05 18:43:03 -0500

    Replaced tab with 4-space

--- evaluation.py
+++ evaluation.py
@@ -5,46 +5,45 @@ from job import Job
 
 
 def evaluate():
-	matrix = numpy.array(list(csv.reader(open("eval.csv","rb"),delimiter=','))).astype('string')
+    matrix = numpy.array(list(csv.reader(open("eval.csv","rb"),delimiter=','))).astype('string')
+    retrieved = []
 
-	retrieved = []
+    for i in range(1,len(matrix)):
+        print matrix[i][1]
+        job  = Job(matrix[i][1])
+        urls = job.execute()
 
-	for i in range(1,len(matrix)):
-		print matrix[i][1]
-		job  = Job(matrix[i][1])
-		urls = job.execute()
+        
+        url1Retrieved  = 0
+        url2Retrieved = 0
+        url3Retrieved = 0
+        for url in urls:
+            #print url
+            if (url == matrix[i][3] ):
+                url1Retrieved  = 1
+            if (url == matrix[i][4] ):
+                url2Retrieved  = 1
+            if (url == matrix[i][5] ):
+                url3Retrieved  = 1
 
-		
-		url1Retrieved  = 0
-		url2Retrieved = 0
-		url3Retrieved = 0
-		for url in urls:
-			#print url
-			if (url == matrix[i][3] ):
-				url1Retrieved  = 1
-			if (url == matrix[i][4] ):
-				url2Retrieved  = 1
-			if (url == matrix[i][5] ):
-				url3Retrieved  = 1
+        if (matrix[i][5] == ''):
+            length = 2
+        else:
+            length = 3
 
-		if (matrix[i][5] == ''):
-			length = 2
-		else:
-			length = 3
-
-		recall = float(url1Retrieved+url2Retrieved+url3Retrieved)/length
-		# if url1Retrieved+url2Retrieved+url3Retrieved > 0:
-		# 	recall = 1
-		# else:
-		# 	recall = 0
-		retrieved.append(recall)
-
-
-	print retrieved
-
-		
+        recall = float(url1Retrieved+url2Retrieved+url3Retrieved)/length
+        # if url1Retrieved+url2Retrieved+url3Retrieved > 0:
+        #     recall = 1
+        # else:
+        #     recall = 0
+        retrieved.append(recall)
 
 
-	
+    print retrieved
+
+        
+
+
     
-	
+    
+    
