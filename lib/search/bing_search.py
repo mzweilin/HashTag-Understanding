75f5a93d266a58301e68901d7f1e3aba492ea5b4
Weilin <mzweilin@gmail.com>
2014-12-05 16:06:46 -0500

    Implemented group search and result merging.

--- lib/search/bing_search.py
+++ lib/search/bing_search.py
@@ -1,12 +1,14 @@
+from collections import Counter
 from bing_search_api import BingSearchAPI
 my_key = "MEL5FOrb1H5G1E78YY8N5mkfcvUK2hNBYsZl1aAEEbE"
- 
+bing = BingSearchAPI(my_key)
+
 def main():
     print(search("Hello", 3))
 
 # category='Web' or category='News'
 def search(query_string, num_results, category='Web'):
-    bing = BingSearchAPI(my_key)
+    query_string = query_string.replace("#", "%23")
     params = {'ImageFilters':'"Face:Face"',
               '$format': 'json',
               '$top': num_results,
@@ -14,6 +16,20 @@ def search(query_string, num_results, category='Web'):
     results = bing.search(category,query_string,params).json() # requests 1.0+ 
 
     return [result['Url'] for result in results['d']['results'][0][category]]
- 
+
+def group_search(query_list, num_results, category='Web', on_wiki=True, exclude_twitter=True):
+    url_counter = Counter()
+    for query in query_list:
+        if exclude_twitter == True:
+            query += " -site:twitter.com"
+        if on_wiki == True:
+            query += " site:wikipedia.org"
+        urls = search(query, num_results)
+        for i in range(len(urls)):
+            url = urls[i]
+            url_counter[url] += float(1)/3**float(i)
+        
+    return [tup[0] for tup in url_counter.most_common(num_results)]
+
 if __name__ == "__main__":
     main()
--- test_3.py
+++ test_3.py
@@ -1,6 +1,4 @@
 import lib.search.bing_search as bing
-import string
-from collections import Counter
 
 # def match_url_keywords(url, keywords):
 #     sentences = get_sentences_from_url(url)
@@ -14,26 +12,12 @@ from collections import Counter
 
 "http://www.wikipedia.org/"
 
-url_counter = Counter()
 # keywords = ['twitter', 'blades', 'sufc']
 #queries = ['#twitterblades', 'sufc', 'twitterblades sufc', 'sufc twitterblades']
 #queries = ['#pangu', 'pangu jailbreak', 'jailbreak pangu']
 queries = ['#pangu', 'jailbreak', 'jailbreak pangu', 'jailbreak pangu']
 #queries = ['']
 #queries = ['#pangu']
-for query in queries:
-    if isinstance(query, list):
-        query = " ".join(query)
-    query = query.replace("#", "%23")
-    print query
-    on_wiki = False
-    query += " -site:twitter.com"
-    if on_wiki == True:
-        query += " site:wikipedia.org"
-    urls = bing.search(query, 20)
-    for i in range(len(urls)):
-        url = urls[i]
-        print url
-        url_counter[url] += float(1)/3**float(i)
-print url_counter.most_common(10)
 
+urls = bing.group_search(queries, 2)
+print urls
